318e77c5871c3f7da5eaa2805c44f88cdc09b607
Carolo Rios <Meli@Meli>
2017-11-22 13:56:21 -0300

    New Tweet Manager with more tweets

--- src/service/tweet_manager.go
+++ src/service/tweet_manager.go
@@ -6,29 +6,42 @@ import (
 	"github.com/TallerGo/src/domain"
 )
 
-var tweet *domain.Tweet
+var tweets []*domain.Tweet
 
 //PublishTweet publish a tweet
-func PublishTweet(aTweet *domain.Tweet) error {
-	if aTweet.User == "" {
+func PublishTweet(tweet *domain.Tweet) error {
+	if tweet.User == "" {
 		return fmt.Errorf("user is required")
 	}
-	if aTweet.Text == "" {
+	if tweet.Text == "" {
 		return fmt.Errorf("text is required")
 	}
-	if len(aTweet.Text) > 140 {
-		return fmt.Errorf("text exceeding 140 characters")
+	if len(tweet.Text) > 140 {
+		return fmt.Errorf("text exceeds 140 characters")
 	}
-	tweet = aTweet
+	tweets = append(tweets, tweet)
 	return nil
 }
 
 //GetTweet return the tweet
 func GetTweet() *domain.Tweet {
-	return tweet
+	if len(tweets) != 0 {
+		return tweets[len(tweets)-1]
+	}
+	return nil
 }
 
 //ClearTweet clear a tweet
 func ClearTweet() {
-	tweet = nil
+	tweets = tweets[:0]
+}
+
+//InitializeService initial service
+func InitializeService() {
+	tweets = make([]*domain.Tweet, 0)
+}
+
+//GetTweets return the tweets
+func GetTweets() []*domain.Tweet {
+	return tweets
 }
--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -9,6 +9,9 @@ import (
 
 func TestPublishedTweetIsSaved(t *testing.T) {
 
+	// Initialization
+	service.InitializeService()
+
 	var tweet *domain.Tweet
 
 	user := "grupoesfera"
@@ -16,22 +19,152 @@ func TestPublishedTweetIsSaved(t *testing.T) {
 
 	tweet = domain.NewTweet(user, text)
 
+	// Operation
 	service.PublishTweet(tweet)
 
+	// Validation
 	publishedTweet := service.GetTweet()
 
-	if publishedTweet.User != user &&
-		publishedTweet.Text != text {
-		t.Errorf("Expected tweet is %s: %s \nbut is %s: %s",
-			user, text, publishedTweet.User, publishedTweet.Text)
-	}
+	isValidTweet(t, publishedTweet, user, text)
+}
 
-	if publishedTweet.Date == nil {
-		t.Error("Expected date cannot be nil")
+func TestTweetWithoutUserIsNotPublished(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+
+	var tweet *domain.Tweet
+
+	var user string
+	text := "This is my first tweet"
+
+	tweet = domain.NewTweet(user, text)
+
+	// Operation
+	var err error
+	err = service.PublishTweet(tweet)
+
+	// Validation
+	if err != nil && err.Error() != "user is required" {
+		t.Error("Expected error is user is required")
 	}
 }
+
+func TestTweetWithoutTextIsNotPublished(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+
+	var tweet *domain.Tweet
+
+	user := "grupoesfera"
+	var text string
+
+	tweet = domain.NewTweet(user, text)
+
+	// Operation
+	var err error
+	err = service.PublishTweet(tweet)
+
+	// Validation
+	if err == nil {
+		t.Error("Expected error")
+		return
+	}
+
+	if err.Error() != "text is required" {
+		t.Error("Expected error is text is required")
+	}
+}
+
+func TestTweetWhichExceeding140CharactersIsNotPublished(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+
+	var tweet *domain.Tweet
+
+	user := "grupoesfera"
+	text := `The Go project has grown considerably with over half a million users and community members 
+		all over the world. To date all community oriented activities have been organized by the community
+		with minimal involvement from the Go project. We greatly appreciate these efforts`
+
+	tweet = domain.NewTweet(user, text)
+
+	// Operation
+	var err error
+	err = service.PublishTweet(tweet)
+
+	// Validation
+	if err == nil {
+		t.Error("Expected error")
+		return
+	}
+
+	if err.Error() != "text exceeds 140 characters" {
+		t.Error("Expected error is text exceeds 140 characters")
+	}
+}
+func TestCanPublishAndRetrieveMoreThanOneTweet(t *testing.T) {
+
+	// Initialization
+	service.InitializeService()
+
+	var tweet, secondTweet *domain.Tweet
+
+	user := "grupoesfera"
+	text := "This is my first tweet"
+	secondText := "This is my second tweet"
+
+	tweet = domain.NewTweet(user, text)
+	secondTweet = domain.NewTweet(user, secondText)
+
+	// Operation
+	service.PublishTweet(tweet)
+	service.PublishTweet(secondTweet)
+
+	// Validation
+	publishedTweets := service.GetTweets()
+
+	if len(publishedTweets) != 2 {
+
+		t.Errorf("Expected size is 2 but was %d", len(publishedTweets))
+		return
+	}
+
+	firstPublishedTweet := publishedTweets[0]
+	secondPublishedTweet := publishedTweets[1]
+
+	if !isValidTweet(t, firstPublishedTweet, user, text) {
+		return
+	}
+
+	if !isValidTweet(t, secondPublishedTweet, user, secondText) {
+		return
+	}
+
+}
+
+func isValidTweet(t *testing.T, tweet *domain.Tweet, user, text string) bool {
+
+	if tweet.User != user && tweet.Text != text {
+		t.Errorf("Expected tweet is %s: %s \nbut is %s: %s",
+			user, text, tweet.User, tweet.Text)
+		return false
+	}
+
+	if tweet.Date == nil {
+		t.Error("Expected date can't be nil")
+		return false
+	}
+
+	return true
+
+}
+
 func TestCleanedTweetIsClean(t *testing.T) {
 
+	service.InitializeService()
 	var tweet *domain.Tweet
 
 	user := "grupoesfera"
@@ -42,65 +175,7 @@ func TestCleanedTweetIsClean(t *testing.T) {
 	service.PublishTweet(tweet)
 	service.ClearTweet()
 
-	if service.GetTweet() != nil {
+	if len(service.GetTweets()) != 0 {
 		t.Error("Expected empty tweet")
-	}
-}
-
-func TestTweetWithoutUserIsNotPublished(t *testing.T) {
-	var tweet *domain.Tweet
-
-	var user string
-	text := "This is my first tweet"
-
-	tweet = domain.NewTweet(user, text)
-
-	var err error
-	err = service.PublishTweet(tweet)
-
-	if err != nil && err.Error() != "user is required" {
-		t.Error("Expected error is user is required")
-	}
-}
-
-func TestTweetWithoutTextIsNotPublished(t *testing.T) {
-	var tweet *domain.Tweet
-
-	var user string
-	user = "grupoesfera"
-	var text string
-
-	tweet = domain.NewTweet(user, text)
-
-	var err error
-	err = service.PublishTweet(tweet)
-
-	if err == nil {
-		t.Error("Expected error")
-	}
-	if err.Error() != "text is required" {
-		t.Error("Expected error is text is required")
-	}
-}
-
-func TestTweetWhichExceeding140CharactersIsNotPublished(t *testing.T) {
-	var tweet *domain.Tweet
-	user := "grupoesfera"
-
-	text := `Whether you use Waterfall, Agile, or Conversational Development, GitLab streamlines your collaborative workflows. 
-	Visualize, prioritize, coordinate, and track your progress your way with GitLab’s flexible project management tools. 
-	Spend less time configuring your tools, and more time creating. Whether you’re deploying to one server or thousands, 
-	build, test, and release your code confidently and securely with GitLab’s built-in continuous delivery and deployment.`
-
-	tweet = domain.NewTweet(user, text)
-
-	var err error
-	err = service.PublishTweet(tweet)
-
-	if err == nil {
-		t.Error("Expected error")
-	}
-	if err.Error() != "text exceeding 140 characters" {
-		t.Error("Expected error is text exceeding 140 characters")
 	}
 }
